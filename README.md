34b21dcbcb36998a497b6a50a252472f48ccfb10
Dass1010 <arokiadass260@gmail.com>
2024-03-19 11:15:26 +0530

    login form created

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularFileStructure
+# Facebook Login Page Clone -Web
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
 
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 let MATERIAL_MODULES = [
   MatToolbarModule,
   MatIconModule,
@@ -15,7 +14,7 @@ let MATERIAL_MODULES = [
   MatFormFieldModule,
   MatInputModule,
   ReactiveFormsModule,
-  FormsModule
+  FormsModule,
 ];
 
 @NgModule({
